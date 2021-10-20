59bce495b9a94fd6a8e7dcbe2f6d6d3716d1443e
Настя <ablovatskaja@gmail.com>
2021-10-20 16:30:02 +0300

    Add nav

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+import s from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-           <NavLink to={'/pre-junior'}>PreJunior</NavLink>
-            <NavLink to={'/junior'}>Junior</NavLink>
-            <NavLink to={'/junior-plus'}>JuniorPlus</NavLink>
-            {/*<NavLink to={'/Error404'}>Error404</NavLink>*/}
-        </div>
-    )
+
+
+    return (<>
+        <nav className={s.Nav}>
+            <NavLink to={'/pre-junior'} activeClassName={s.activeLink}>PreJunior</NavLink>
+            <NavLink to={'/junior'} activeClassName={s.activeLink}>Junior</NavLink>
+            <NavLink to={'/junior-plus'} activeClassName={s.activeLink}>JuniorPlus</NavLink>
+            <span className={s.toggle}> </span>
+
+        </nav>
+
+
+        {/*<NavLink to={'/Error404'}>Error404</NavLink>*/}
+    </>)
 }
 
 export default Header;
